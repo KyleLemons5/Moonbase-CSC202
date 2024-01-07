264170c60a216e41f6d5d886af5d12aa84a9598d
Kyle Lemons <155856635+KyleLemons5@users.noreply.github.com>
2024-01-06 18:55:28 -0700

    Assignment 7.1: Added an actor class
    Originally done on 10/26/23
    Added an actor class to represent the user and other NPCs. The NPCs are not yet displayed in the program, but are coded in.

--- Moonbase/Moonbase/FormMain.cs
+++ Moonbase/Moonbase/FormMain.cs
@@ -1,7 +1,7 @@
 ﻿// Kyle Lemons
 // CSC202
 // 9/19/23
-// Last Edited: 10/17/23
+// Last Edited: 10/26/23
 /*
  *                           Map (not to scale)
  *                             ______________
@@ -26,6 +26,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Media;
 using System.Text;
 using System.Windows.Forms;
 
@@ -35,8 +36,6 @@ namespace Moonbase
     public partial class FormMain : Form
     {
 
-        private string location; // Current location of the user
-
         // All locations in the moonbase
         Location readingRoom = new Location("Reading Room", "A small room near the corner of the base. This room is primarily used as a quiet room far away from the main common areas. Many people come here to read, but others use this place as a private area to play games or have private conversations.", "Resources\\Moonbase Room.jpg", false, false, true, true);
         Location hallway = new Location("Hallway", "The hallway between the bedrooms of Craterville's residents. The hallway also leads to the dining room and reading room. Many people come through this place every day. It is standard protocol to greet anyone you way see in these halls to avoid potentail interpersonal issues.", "Resources\\Moonbase Hall.jpg", true, true, true, true);
@@ -44,6 +43,14 @@ namespace Moonbase
         Location outside = new Location("Outside", "Only a small layer of spacesuit protects you from the cold embrace of space. Outside the base is completely silent due to a lask of atmosphere. The moondust is a sharp powder due to millions of small meteorite impacts and no atmosphere to errode away the sharp edges. The dust is also electrostatically charged, sticking to any surface it comes into contact with. There's nothing to do out here except check on equipment occasionally.", "Resources\\Moonbase Outside.jpg", false, false, true, false);
         Location northBedroom = new Location("North Bedroom", "The bedrooms in the northern section of Craterville. Higher ranking offcials get their own private bedrooms here. Some offcials use their bedrooms as personal offices.", "Resources\\Moonbase Bedroom 1.jpg", false, true, false, false);
         Location southBedroom = new Location("South Bedroom", "The bedrroms in the southern section od Craterville. Here several beds are packed into the same rooms to save space. Some people host lite night gatherings here with thier roommates.", "Resources\\Moonbase Bedroom 2.jpg", true, false, false, false);
+        
+        Actor user = new Actor("Reading Room"); // User Actor
+
+        // All NPC's in the moonbase (not displayed yet)
+        NPC Jim = new NPC("North bedroom", "Jim", "Captain");
+        NPC Jimbo = new NPC("South bedroom", "Jimbo", "Engineer");
+        NPC Jimmy = new NPC("Hallway", "Jimmy", "Engineer");
+        NPC Jimmothy = new NPC("Dining Room", "Jimmothy", "Chef");
 
         public FormMain()
         {
@@ -53,7 +60,7 @@ namespace Moonbase
         // "Moves" the user to the location given to the method
         private void moveTo(Location room)
         {
-            this.location = room.getLocation();
+            user.setLocation(room.getLocation());
             textBoxName.Text = room.getLocation();
             textBoxDesc.Text = room.getDescription();
             ActiveForm.BackgroundImage = Image.FromFile(room.getImage());
@@ -65,7 +72,7 @@ namespace Moonbase
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            location = textBoxName.Text; // Upon loading the form, set the location to the current location text name
+            user.setLocation(textBoxName.Text); // Upon loading the form, set the location to the current location text name
             northButton.Enabled = false;
             southButton.Enabled = false;
         }
@@ -73,17 +80,17 @@ namespace Moonbase
         // "Moves" the user to the west by by calling the correct room update
         private void westButton_Click(object sender, EventArgs e)
         {
-            if (location == "Reading Room")
+            if (user.getLocation() == "Reading Room")
             {
                 moveTo(hallway);
             }
 
-            else if (location == "Hallway")
+            else if (user.getLocation() == "Hallway")
             {
                 moveTo(diningRoom);
             }
 
-            else if (location == "Outside")
+            else if (user.getLocation() == "Outside")
             {
                 moveTo(readingRoom);
             }
@@ -92,17 +99,17 @@ namespace Moonbase
         // "Moves" the user to the east by by calling the correct room update
         private void eastButton_Click(object sender, EventArgs e)
         {
-            if (location == "Reading Room")
+            if (user.getLocation() == "Reading Room")
             {
                 moveTo(outside);
             }
 
-            else if (location == "Hallway")
+            else if (user.getLocation() == "Hallway")
             {
                 moveTo(readingRoom);
             }
 
-            else if (location == "Dining Room")
+            else if (user.getLocation() == "Dining Room")
             {
                 moveTo(hallway);
             }
@@ -111,12 +118,12 @@ namespace Moonbase
         // "Moves" the user to the north by by calling the correct room update
         private void northButton_Click(object sender, EventArgs e)
         {
-            if (location == "Hallway")
+            if (user.getLocation() == "Hallway")
             {
                 moveTo(northBedroom);
             }
 
-            else if (location == "South Bedroom")
+            else if (user.getLocation() == "South Bedroom")
             {
                 moveTo(hallway);
             }
@@ -125,12 +132,12 @@ namespace Moonbase
         // "Moves" the user to the south by by calling the correct room update
         private void southButton_Click(object sender, EventArgs e)
         {
-            if (location == "Hallway")
+            if (user.getLocation() == "Hallway")
             {
                 moveTo(southBedroom);
             }
 
-            else if (location == "North Bedroom")
+            else if (user.getLocation() == "North Bedroom")
             {
                 moveTo(hallway);
             }
@@ -232,6 +239,66 @@ namespace Moonbase
             return east;
         }
 
+    }
+
+    // Actor class that represents a being in the moonbase
+    public class Actor
+    {
+        // The properties of Actor
+        string location;
+
+        // Constructor of Actor that gets and stores all the information of an actor
+        public Actor(string location)
+        {
+            this.location = location;
+        }
+
+        // Setters and getters for the properties of Actor
+        public void setLocation(String location)
+        {
+            this.location = location;
+        }
+
+        public string getLocation()
+        {
+            return location;
+        }
+    }
+
+    // NPC class that represents NPCs in the moonbase
+    public class NPC : Actor
+    {
+        // The properties of NPC
+        string name;
+        string job;
+
+        // Constructor of NPC that gets and stores all the information of an NPC
+        public NPC(string location, string name, string job) : base(location)
+        {
+            this.name = name;
+            this.job = job;
+        }
+
+        // Setters and getters for the properties of NPC
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setJob(String job)
+        {
+            this.job = job;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getJob()
+        {
+            return job;
+        }
     }
 
 }
