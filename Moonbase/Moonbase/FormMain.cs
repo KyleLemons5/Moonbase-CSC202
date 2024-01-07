c8c5d0bad90b76a4d75fe3f1b48deefc52ac7412
Kyle Lemons <155856635+KyleLemons5@users.noreply.github.com>
2024-01-06 18:52:53 -0700

    Assignment 6.1: Added classes and objects
    Originally done on 10/17/23
    The functionality of the program is the same, but many variables in the code have been replaced with objects.

--- Moonbase/Moonbase/FormMain.cs
+++ Moonbase/Moonbase/FormMain.cs
@@ -1,12 +1,12 @@
 ﻿// Kyle Lemons
 // CSC202
 // 9/19/23
-// Last Edited: 10/10/23
+// Last Edited: 10/17/23
 /*
  *                           Map (not to scale)
  *                             ______________
  *                             |            |
- *                             |  Bedroom?  |
+ *                             |  Bedroom   |
  *                             |__        __|
  * __________   ______________   |        |   _______________
  * |        |___|            |___|        |___|             |__
@@ -14,7 +14,7 @@
  * |________|   |____________|   |        |   |_____________|
  *                             __|        |__
  *                             |            |
- *                             |  Bedroom?  |
+ *                             |  Bedroom   |
  *                             |____________|
  * 
  * Note: Locations with "?" are not currently implemented
@@ -25,8 +25,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
+
 
 namespace Moonbase
 {
@@ -35,80 +37,30 @@ namespace Moonbase
 
         private string location; // Current location of the user
 
+        // All locations in the moonbase
+        Location readingRoom = new Location("Reading Room", "A small room near the corner of the base. This room is primarily used as a quiet room far away from the main common areas. Many people come here to read, but others use this place as a private area to play games or have private conversations.", "Resources\\Moonbase Room.jpg", false, false, true, true);
+        Location hallway = new Location("Hallway", "The hallway between the bedrooms of Craterville's residents. The hallway also leads to the dining room and reading room. Many people come through this place every day. It is standard protocol to greet anyone you way see in these halls to avoid potentail interpersonal issues.", "Resources\\Moonbase Hall.jpg", true, true, true, true);
+        Location diningRoom = new Location("Dining Room", "This is the most active room in all of Craterville. Residents come to eat and enjoy eachother's company. The food selection is quite limited with only dehydrated foods being available. This room is also often host to larger group activities and meetings.", "Resources\\Moonbase Dining.jpg", false, false, false, true);
+        Location outside = new Location("Outside", "Only a small layer of spacesuit protects you from the cold embrace of space. Outside the base is completely silent due to a lask of atmosphere. The moondust is a sharp powder due to millions of small meteorite impacts and no atmosphere to errode away the sharp edges. The dust is also electrostatically charged, sticking to any surface it comes into contact with. There's nothing to do out here except check on equipment occasionally.", "Resources\\Moonbase Outside.jpg", false, false, true, false);
+        Location northBedroom = new Location("North Bedroom", "The bedrooms in the northern section of Craterville. Higher ranking offcials get their own private bedrooms here. Some offcials use their bedrooms as personal offices.", "Resources\\Moonbase Bedroom 1.jpg", false, true, false, false);
+        Location southBedroom = new Location("South Bedroom", "The bedrroms in the southern section od Craterville. Here several beds are packed into the same rooms to save space. Some people host lite night gatherings here with thier roommates.", "Resources\\Moonbase Bedroom 2.jpg", true, false, false, false);
+
         public FormMain()
         {
             InitializeComponent();
         }
 
-        // "Moves" the user to the Reading Room by updating all relevant components to the Reading Room settings
-        private void moveToReadingRoom()
+        // "Moves" the user to the location given to the method
+        private void moveTo(Location room)
         {
-            location = "Reading Room";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "A small room near the corner of the base. This room is primarily used as a quiet room far away from the main common areas. Many people come here to read, but others use this place as a private area to play games or have private conversations.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Room;
-            eastButton.Enabled = true;
-            northButton.Enabled = false;
-            southButton.Enabled = false;
-        }
-
-        // "Moves" the user to the Hallway by updating all relevant components to the Hallway settings
-        private void moveToHallway()
-        {
-            location = "Hallway";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "The hallway between the bedrooms of Craterville's residents. The hallway also leads to the dining room and reading room. Many people come through this place every day. It is standard protocol to greet anyone you way see in these halls to avoid potentail interpersonal issues.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Hall;
-            northButton.Enabled = true;
-            southButton.Enabled = true;
-            westButton.Enabled = true;
-            eastButton.Enabled = true;
-        }
-
-        // "Moves" the user to the Dining Room by updating all relevant components to the Dining Room settings
-        private void moveToDining()
-        {
-            location = "Dining Room";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "This is the most active room in all of Craterville. Residents come to eat and enjoy eachother's company. The food selection is quite limited with only dehydrated foods being available. This room is also often host to larger group activities and meetings.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Dining;
-            northButton.Enabled = false;
-            southButton.Enabled = false;
-            westButton.Enabled = false;
-        }
-
-        // "Moves" the user to the Outside by updating all relevant components to the Outside settings
-        private void moveToOutside()
-        {
-            location = "Outside";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "Only a small layer of spacesuit protects you from the cold embrace of space. Outside the base is completely silent due to a lask of atmosphere. The moondust is a sharp powder due to millions of small meteorite impacts and no atmosphere to errode away the sharp edges. The dust is also electrostatically charged, sticking to any surface it comes into contact with. There's nothing to do out here except check on equipment occasionally.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Outside;
-            eastButton.Enabled = false;
-        }
-
-        // "Moves" the user to the North Bedroom by updating all relevant components to the North Bedroom settings
-        private void moveToNorthBedroom()
-        {
-            location = "North Bedroom";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "The bedrooms in the northern section of Craterville. Higher ranking offcials get their own private bedrooms here. Some offcials use their bedrooms as personal offices.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Bedroom_1;
-            northButton.Enabled = false;
-            eastButton.Enabled = false;
-            westButton.Enabled = false;
-        }
-
-        // "Moves" the user to the South Bedroom by updating all relevant components to the South Bedroom settings
-        private void moveToSouthBedroom()
-        {
-            location = "South Bedroom";
-            textBoxName.Text = location;
-            textBoxDesc.Text = "The bedrroms in the southern section od Craterville. Here several beds are packed into the same rooms to save space. Some people host lite night gatherings here with thier roommates.";
-            ActiveForm.BackgroundImage = Properties.Resources.Moonbase_Bedroom_1;
-            southButton.Enabled = false;
-            eastButton.Enabled = false;
-            westButton.Enabled = false;
+            this.location = room.getLocation();
+            textBoxName.Text = room.getLocation();
+            textBoxDesc.Text = room.getDescription();
+            ActiveForm.BackgroundImage = Image.FromFile(room.getImage());
+            northButton.Enabled = room.getNorth();
+            southButton.Enabled = room.getSouth();
+            westButton.Enabled = room.getWest();
+            eastButton.Enabled = room.getEast();
         }
 
         private void FormMain_Load(object sender, EventArgs e)
@@ -123,17 +75,17 @@ namespace Moonbase
         {
             if (location == "Reading Room")
             {
-                moveToHallway();
+                moveTo(hallway);
             }
 
             else if (location == "Hallway")
             {
-                moveToDining();
+                moveTo(diningRoom);
             }
 
             else if (location == "Outside")
             {
-                moveToReadingRoom();
+                moveTo(readingRoom);
             }
         }
 
@@ -142,17 +94,17 @@ namespace Moonbase
         {
             if (location == "Reading Room")
             {
-                moveToOutside();
+                moveTo(outside);
             }
 
             else if (location == "Hallway")
             {
-                moveToReadingRoom();
+                moveTo(readingRoom);
             }
 
             else if (location == "Dining Room")
             {
-                moveToHallway();
+                moveTo(hallway);
             }
         }
 
@@ -161,12 +113,12 @@ namespace Moonbase
         {
             if (location == "Hallway")
             {
-                moveToNorthBedroom();
+                moveTo(northBedroom);
             }
 
             else if (location == "South Bedroom")
             {
-                moveToHallway();
+                moveTo(hallway);
             }
         }
 
@@ -175,13 +127,111 @@ namespace Moonbase
         {
             if (location == "Hallway")
             {
-                moveToSouthBedroom();
+                moveTo(southBedroom);
             }
 
             else if (location == "North Bedroom")
             {
-                moveToHallway();
+                moveTo(hallway);
             }
         }
     }
+
+    // Class that creates a location object that stores all the information of a location
+    public class Location
+    {
+        // The properties of Location
+        private string location;
+        private string description;
+        private string image;
+        private Boolean north;
+        private Boolean south;
+        private Boolean west;
+        private Boolean east;
+
+        // Constructor of Location that gets and stores all the information of a location
+        public Location(string location, string description, string image, bool north, bool south, bool west, bool east)
+        {
+            this.location = location;
+            this.description = description;
+            image = Path.GetFullPath(image); // Gets the full path of an image, so it can be correctly called later
+            this.image = image;
+            this.north = north;
+            this.south = south;
+            this.west = west;
+            this.east = east;
+        }
+
+        // Setters and getters for the properties of Location
+        public void setLocation(string location)
+        {
+            this.location = location;
+        }
+
+        public void setDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public void setImage(string image)
+        {
+            this.image = image;
+        }
+
+        public void setNorth(Boolean north)
+        {
+            this.north = north;
+        }
+
+        public void setSouth(Boolean south)
+        {
+            this.south = south;
+        }
+
+        public void setWeat(Boolean west)
+        {
+            this.west = west;
+        }
+
+        public void setEast(Boolean east)
+        {
+            this.east = east;
+        }
+
+        public string getLocation()
+        {
+            return location;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+        public string getImage()
+        {
+            return image;
+        }
+
+        public Boolean getNorth()
+        {
+            return north;
+        }
+
+        public Boolean getSouth()
+        {
+            return south;
+        }
+
+        public Boolean getWest()
+        {
+            return west;
+        }
+
+        public Boolean getEast()
+        {
+            return east;
+        }
+
+    }
+
 }
